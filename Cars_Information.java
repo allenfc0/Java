56a67c66efca1c318e29e77d9b6204b16781a1c8
Allen <44152004+allenfc0@users.noreply.github.com>
2021-06-14 21:37:12 -0700

    Add files via upload

--- Cars_Information.java
+++ Cars_Information.java
@@ -1,5 +1,32 @@
 package Assignment;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Cars_Information {
+	public static void main(String [] args) {
+	String path = "D:\\Program Files\\PerScholas\\Week 3-Java\\Res\\cars.csv";
 	
+	
+	try {
+		File doc = new File(path);
+		Scanner sc = new Scanner(doc);
+		String[] arr = null;
+		int i = 0;
+		while(sc.hasNextLine()) {
+			arr = sc.nextLine().split(" ,");
+			System.out.println(arr[i]);
+			i++;
+		}
+		
+//		for(String x : arr) {
+//			System.out.println(x);
+//		}
+		sc.close();
+		
+		
+	}catch(Exception e) {
+		
+	}
+	}
 }

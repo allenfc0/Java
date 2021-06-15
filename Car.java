0031de50ce25b35cc7736048f8074807aaac192b
Allen <44152004+allenfc0@users.noreply.github.com>
2021-06-14 19:51:02 -0700

    Add files via upload

--- Car.java
+++ Car.java
@@ -2,4 +2,19 @@ package Assignment;
 
 public class Car {
 	
+	private String car;
+	private double mpg;
+	private int cylinders;
+	private double displacement;
+	private double horsepower;
+	private double weight;
+	private double acceleration;
+	private int model;
+	private String origin;
+	
+	public Car() {
+		car = "";
+		mpg = 0;
+		
+	}
 }
